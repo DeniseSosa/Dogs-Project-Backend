3cb3ee8a675caba2d7453196522088a921cdc9c7
unknown <rocio.sosa94@gmail.com>
2024-09-24 16:54:08 -0300

    deploy

--- api/src/db.js
+++ api/src/db.js
@@ -6,11 +6,16 @@ const {
    DB_USER,
    DB_PASSWORD, 
    DB_HOST,
+   DB_DEPLOY
 } = process.env;
 const DogModel= require('./models/Dog');
 const TemperamentsModel= require('./models/Temperaments');
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
+// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
+//   logging: false, // set to console.log to see the raw SQL queries
+//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+// });
+const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
